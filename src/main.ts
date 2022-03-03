74b2cc22c00e657c8b476f80e1f44579d0632ce9
Yasha <ysahakyan1998@gmail.com>
2022-03-03 18:11:06 +0400

    messages

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  await app.listen(4000);
+}
+
+bootstrap();
